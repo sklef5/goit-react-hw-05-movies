35af5e46fc4dd73921472f543c2a298562a7dbff
DVorortnoy <dimchik505@gmail.com>
2023-04-06 23:12:30 +0300

    add

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -3,26 +3,28 @@ import { useEffect, useState } from 'react';
 import { findMovies } from '../service/API';
 import { ListMovie } from '../ListMovie/ListMovie';
 import s from './SearchBar.module.scss';
-
+import { Notify } from 'notiflix';
 const SearchBar = () => {
   const [input, setInput] = useState('');
   const [search, setSearch] = useSearchParams();
   const query = search.get('query');
-  const [listMovies, setListMovies] = useState([]);
+  const [listMovies, setListMovies] = useState(null);
 
   const handleSubmit = e => {
     e.preventDefault();
     setSearch({ query: input });
     setInput('');
-    
   };
 
   useEffect(() => {
     if (!query) return;
     findMovies(query)
-      .then(data => setListMovies(data))
+      .then(data => {
+        data.length !== 0 ? setListMovies(data) : Notify.failure('Not such movie');
+      })
       .catch(err => {
-        console.log(err)});
+        console.log(err);
+      });
   }, [query]);
 
   return (
